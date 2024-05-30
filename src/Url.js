d3cedf87114e5e7694df1d68a7a4f94fbfd9a893
navalrahman <77826561+navalrahman@users.noreply.github.com>
2024-05-30 19:09:27 +0530

    Update Url.js

--- src/Url.js
+++ src/Url.js
@@ -1,3 +1,4 @@
-export const baseUrl = 'http://localhost:4000'
+export const baseUrl = 'http://localhost:4000' || 'https://mernbe-1207.onrender.com'
+
 // export const baseUrl = 'https://mernbe-1207.onrender.com'
 // export const baseUrl = 'https://mernbe-1207.onrender.com/' // || 'https://mernbe-3c3aiy75v-naval-rahman-ks-projects.vercel.app/'
